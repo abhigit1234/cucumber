513bc1e103c1e8734f056d9e277203a8fce86c39
abhigit1234 <150925884+abhigit1234@users.noreply.github.com>
2024-04-03 14:14:31 +0530

    Update runner.java

--- src/test/java/RunnerFile/runner.java
+++ src/test/java/RunnerFile/runner.java
@@ -6,8 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-			
+@CucumberOptions(			
 			features = "src/test/resources/Features/re.feature",
 			glue = "StepFile",
 			tags = "",
